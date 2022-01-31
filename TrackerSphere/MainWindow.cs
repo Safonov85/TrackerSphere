9308cccad642c79274362e649dfef91ea24c1ff5
Safonov85 <safonov1985@protonmail.com>
2022-01-31 14:53:36 +0100

    Random dot painter, with colors

--- TrackerSphere/MainWindow.cs
+++ TrackerSphere/MainWindow.cs
@@ -3,13 +3,20 @@ using Cairo;
 using Gtk;
 
 
+
 public partial class MainWindow : Gtk.Window
 {
     SimulateSpheres simSphere = new SimulateSpheres();
     PointD sphere;
+    PointD dot;
     double sphereVolume = 50;
     double speed = 20;
     double sphereY;
+    double dotVolume = 3;
+    double dotRed = 0;
+
+    int widthScreen, heightScreen;
+
 
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
@@ -21,7 +28,14 @@ public partial class MainWindow : Gtk.Window
 
         sphere.X = drawingArea.WidthRequest;
         sphere.Y = GiveRandomNum();
-        //sphereY = sphere.Y;
+
+        widthScreen = drawingArea.WidthRequest;
+        heightScreen = drawingArea.HeightRequest;
+
+        
+
+        dotX = drawingArea.WidthRequest / 2;
+        dotY = drawingArea.HeightRequest / 2;
 
         ClockStart();
 
@@ -36,7 +50,7 @@ public partial class MainWindow : Gtk.Window
 
     void ClockStart()
     {
-        GLib.Timeout.Add(20, new GLib.TimeoutHandler(Update));
+        GLib.Timeout.Add(2, new GLib.TimeoutHandler(Update));
     }
 
 
@@ -45,7 +59,8 @@ public partial class MainWindow : Gtk.Window
     {
         DrawBlackScreen();
 
-        CreateCircle();
+        //CreateCircle();
+        CreateDot();
 
 
         return true;
@@ -54,7 +69,7 @@ public partial class MainWindow : Gtk.Window
     void DrawBlackScreen()
     {
         //drawingArea.ModifyBg(StateType.Normal, new Gdk.Color(100, 0, 0));
-        drawingArea.GdkWindow.Clear();
+        //drawingArea.GdkWindow.Clear();
 
         simSphere.DrawSphere(drawingArea.GdkWindow);
     }
@@ -65,6 +80,28 @@ public partial class MainWindow : Gtk.Window
         Random rand = new Random();
 
         num = rand.Next(1, drawingArea.HeightRequest);
+
+        return num;
+
+    }
+
+    int GiveRandomLimited()
+    {
+        int num = new Random().Next(-5, 6);
+
+        if(dotX < 0 || dotY < 0)
+        {
+            num = 1;
+        }
+        if(dotX > widthScreen)
+        {
+            dotX = widthScreen;
+        }
+        if(dotY > heightScreen)
+        {
+            dotY = heightScreen;
+        }
+
 
         return num;
 
@@ -82,17 +119,53 @@ public partial class MainWindow : Gtk.Window
         height = Allocation.Height;
 
         sphere.X = sphere.X - speed;
-        sphereY = 5 + sphereY;
+        //sphereY = 5 + sphereY;
 
 
         circle.Translate(sphere.X, sphere.Y + sphereY);
         circle.Arc(0, 0, sphereVolume, 0, 2 * Math.PI);
         circle.StrokePreserve();
 
-        circle.SetSourceRGB(0.3, 0.4, 0.6);
+        circle.SetSourceRGB(0.0, 0.4, 0.6);
         circle.Fill();
 
         circle.GetTarget().Dispose();
         ((IDisposable)circle).Dispose();
     }
+
+    void CreateDot()
+    {
+        Cairo.Context circle = Gdk.CairoHelper.Create(drawingArea.GdkWindow);
+
+        circle.LineWidth = 0;
+
+
+
+        dotX = GiveRandomLimited() + dotX;
+        dotY = GiveRandomLimited() + dotY;
+
+
+        circle.Translate(dotX, dotY);
+        circle.Arc(0, 0, dotVolume, 0, 2 * Math.PI);
+        circle.StrokePreserve();
+
+
+        dotRed = dotRed + 0.01;
+        if(dotRed > 1.0)
+        {
+            dotRed = 0.0;
+        }
+        circle.SetSourceRGB(dotRed, 0.4, 0.6);
+
+
+        circle.Fill();
+
+        circle.GetTarget().Dispose();
+        ((IDisposable)circle).Dispose();
+    }
+
+    
+
+    public double dotX { get; set; }
+    public double dotY { get; set; }
 }
